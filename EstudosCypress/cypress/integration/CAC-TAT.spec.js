2512dcdd4f16c9743e3b65ab7c8dc2fd631ffefa
Cristiane Araujo Souza Dos Reis <cristianeasreis@hotmail.com>
2024-01-31 15:56:39 -0300

    Exercicio um e dois

--- EstudosCypress/cypress/integration/CAC-TAT.spec.js
+++ EstudosCypress/cypress/integration/CAC-TAT.spec.js
@@ -1,10 +1,19 @@
-{/* <reference types="Cypress" /> */}
+{/* <reference types="Cypress" /> */ }
 
-describe('Central de Atendimento ao Cliente TAT', function() {
-    it('verifica o título da aplicação', function() {
-        cy.visit ('http://127.0.0.1:5500/EstudosCypress/src/')
-
+describe('Central de Atendimento ao Cliente TAT', function () {
+    beforeEach(function () {
+        cy.visit('http://127.0.0.1:5500/EstudosCypress/src/')
+    })
+    it('verifica o título da aplicação', function () {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
-  })
-  
+    it.only('preenche os campos obrigatórios e envia o formulário', function () {
+        cy.get('#firstName').type('Cristiane')
+        cy.get('#lastName').type('Reis')
+        cy.get('#email').type('cristianeasreis@exemplo.com')
+        cy.get('#open-text-area').type('Teste')
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.success').should('be.visible')
+    })
+})
