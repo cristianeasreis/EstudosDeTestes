dee8137d8d752fafc2d31b0a2f9f623f5b4d7c54
Cristiane Araujo Souza Dos Reis <cristianeasreis@hotmail.com>
2024-01-31 17:10:37 -0300

    Exercicio 3

--- EstudosCypress/cypress/integration/CAC-TAT.spec.js
+++ EstudosCypress/cypress/integration/CAC-TAT.spec.js
@@ -7,13 +7,29 @@ describe('Central de Atendimento ao Cliente TAT', function () {
     it('verifica o título da aplicação', function () {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
-    it.only('preenche os campos obrigatórios e envia o formulário', function () {
+    it('preenche os campos obrigatórios e envia o formulário', function () {
+        const LongText = 'Teste, Texto, Teste , Texto,Teste, Texto, Teste , Texto,Teste, Texto, Teste , Texto,Teste, Texto, Teste , Texto,Teste, Texto, Teste , Texto,Teste, Texto, Teste , Texto'
         cy.get('#firstName').type('Cristiane')
         cy.get('#lastName').type('Reis')
         cy.get('#email').type('cristianeasreis@exemplo.com')
+        cy.get('#open-text-area').type(LongText, { delay: 0})
+        cy.get('button[type="submit"]').click()
+        cy.get('.success').should('be.visible')
+    })
+
+    it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function(){
+        cy.get('#firstName').type('Cristiane')
+        cy.get('#lastName').type('Reis')
+        cy.get('#email').type('cristianeasreis@exemplo,com')
         cy.get('#open-text-area').type('Teste')
         cy.get('button[type="submit"]').click()
+        cy.get('.error').should('be.visible')
 
-        cy.get('.success').should('be.visible')
+    })
+
+    it.only('campoo telefone continua vazio quando preenchido com valor não-numerioc', function(){
+        cy.get('#phone')
+        .type('asdfgh')
+        .should('have.value', '')
     })
 })
