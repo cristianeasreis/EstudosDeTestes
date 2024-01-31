5332a99ad9b313d06a5c269a73f6c380950761c0
Cristiane Araujo Souza Dos Reis <cristianeasreis@hotmail.com>
2024-01-31 00:22:42 -0300

    atualização 1

--- EstudosCypress/cypress/integration/CAC-TAT.spec.js
+++ EstudosCypress/cypress/integration/CAC-TAT.spec.js
@@ -2,7 +2,7 @@
 
 describe('Central de Atendimento ao Cliente TAT', function() {
     it('verifica o título da aplicação', function() {
-        cy.visit ('http://127.0.0.1:5500/EstudosCypress/src/index.html')
+        cy.visit ('http://127.0.0.1:5500/EstudosCypress/src/')
 
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
