07c71fdeafef7e6c9189bbdab54e75fe417fbdd7
Cristiane Araujo Souza Dos Reis <cristianeasreis@hotmail.com>
2024-02-02 08:08:09 -0300

    Exercicio de validação de de select

--- EstudosCypress/cypress/integration/CAC-TAT.spec.js
+++ EstudosCypress/cypress/integration/CAC-TAT.spec.js
@@ -64,7 +64,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
             .should('have.value', "")
     })
 
-    it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function(){
+    it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function () {
         cy.contains('button', 'Enviar').click()
         cy.get('.error').should('be.visible')
     })
@@ -74,5 +74,22 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         cy.get('.success').should('be.visible')
     })
 
+    it('seleciona um produto (YouTube) por seu texto', function () {
+        cy.get('#product')
+            .select('YouTube')
+            .should('have.value', 'youtube')
+    })
+
+    it('seleciona um produto (Mentoria) por seu valor (value)', function () {
+        cy.get('#product')
+            .select('mentoria')
+            .should('have.value', 'mentoria')
+    })
+
+    it.only('seleciona um produto (Blog) por seu índice', function () {
+        cy.get('#product')
+            .select(1)
+            .should('have.value', 'blog')
+    })
 
 })
