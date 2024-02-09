0e04f73b722da5a49926c1d66ab7d3e36f5517d3
Cristiane Araujo Souza Dos Reis <cristianeasreis@hotmail.com>
2024-02-09 02:41:25 -0300

    exercicio lodash

--- EstudosCypress/cypress/e2e/privacy.cy.js
+++ EstudosCypress/cypress/e2e/privacy.cy.js
@@ -1,5 +1,7 @@
 
-it('testa a página da política de privacidade de forma independente', function () {
-    cy.visit("http://127.0.0.1:5500/EstudosCypress/src/privacy.htm");
-    cy.contains('Talking About Testing').should('be.visible')
-})
+Cypress._.times(1,function () {
+    it('testa a página da política de privacidade de forma independente', function () {
+        cy.visit("http://127.0.0.1:5500/EstudosCypress/src/privacy.htm");
+        cy.contains('Talking About Testing').should('be.visible')
+    })
+} )
