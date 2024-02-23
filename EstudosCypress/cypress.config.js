69f73efeb06a2aa4faba466681ed6acfbf42bc17
Cristiane Araujo Souza Dos Reis <cristianeasreis@hotmail.com>
2024-02-23 10:36:56 -0300

    exercicio de testes de requisições de api

--- EstudosCypress/cypress.config.js
+++ EstudosCypress/cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   viewportHeight: 768,
-  viewportWidth: 1633,
+  viewportWidth: 1366,
   e2e: {
     setupNodeEvents(on, config) {},
   },
--- EstudosCypress/cypress/e2e/CAC-TAT.cy.js
+++ EstudosCypress/cypress/e2e/CAC-TAT.cy.js
@@ -168,12 +168,24 @@ describe("Central de Atendimento ao Cliente TAT", function () {
       .should('not.be.visible')
   });
 
-  it.only('preenche a area de texto usando o comando invoke', () => {
+  it('preenche a area de texto usando o comando invoke', () => {
     const LongText = Cypress._.repeat('0123456789', 20)
     cy.get('#open-text-area')
       .invoke('val', LongText)
       .should('have.value', LongText)
   });
+
+  it.only('faz uma requisição HTTP', () => {
+    cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+      .should(function (response) {
+        const { status, statusText, body } = response
+        expect(status).to.equal(200)
+        expect(statusText).to.equal('OK')
+        expect(body).to.include('CAC TAT')
+      })
+
+  });
+
 
 });
 
